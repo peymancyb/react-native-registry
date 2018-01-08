e668a3fd33619ee325d48ff87e3cc8738e66d1bd
peymancyb <paymanghazvini@yahoo.com>
2018-01-08 12:43:26 +0400

    seperating components

--- src/FrontEnd/MainPage.js
+++ src/FrontEnd/MainPage.js
@@ -39,37 +39,29 @@ import {student_pal_data} from './mainButtons';
 
 export var resetButtons= false;
 
-export var buttonStyle , buttonState ;
 
 
-export default class MainPage extends Component {
-    constructor(props){
+
+
+class StudentModal extends Component{
+  constructor(props){
     super(props);
-    //solving timer error
-    console.ignoredYellowBox = [
-    'Setting a timer'
-    ];
-    this.state = {
-      name: '',
-      last_name: '',
-      students_array: [],
-      active: false,
-      modalView:false,
-      buttonStyleReset:true,
-      buttonResetActivity:false,
+    this.defaultState={
+        name: '',
+        last_name: '',
+        modalView: props.modalView,
     };
-
-   // this.currentUserUid = FB.auth().currentUser.uid;
-   // this.itemsRef = FB.database().ref('user_classes/'+this.currentUserUid+'/class_list/'+fbDatabaseNodeName+'/studet_list');
-   this.itemsRef = FB.database().ref('user_classes/'+"xuKDcv8itdPnUGhLHjvaWfVEptm2"+'/class_list/'+"First Class"+'/studet_list');
-   this._renderItem = this._renderItem.bind(this);
-   this.listenForItems = this.listenForItems.bind(this);
-   this._saveData = this._saveData.bind(this);
-   this._sendData = this._sendData.bind(this);
-   this._resetButtons = this._resetButtons.bind(this);
-
+    this.state = this.defaultState;
+    this._saveData = this._saveData.bind(this);
+    this._passState = this._passState.bind(this);
   }
 
+  _passState(){
+    this.props.handleState(false);
+    this.setState({
+      modalView: false,
+    });
+  }
   _saveData(){
     if(this.state.name != '' && this.state.last_name != ''){
       this.itemsRef.push({ name: this.state.name, last_name: this.state.last_name});
@@ -90,36 +82,170 @@ export default class MainPage extends Component {
         });
     }
   }
-
-_sendData(){
-  console.log("send data");
-  for(let i=0 ; i< student_pal_data.length;i++){
-    console.log("student: "+student_pal_data[i].user_id);
-    FB.database().ref("test/"+student_pal_data[i].user_id).set({
-      status: student_pal_data[i]
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      modalView: nextProps.modalView
     });
   }
+  render(){
+    return(
+          <View>
+            <Modal
+              animationType="none"
+              transparent={true}
+              visible={this.state.modalView}
+              onRequestClose={()=>this.setState({modalVisible: false})}
+            >
+            <View style={styles.inputcontainerModal}>
+              <TextInput
+                style = {styles.inputStyleModal}
+                onChangeText={(Name) => this.setState({name: Name})}
+                value={this.state.name}
+                placeholder="Name"
+                placeholderTextColor={"white"}
+                underlineColorAndroid={'transparent'}
+                />
+              <TextInput
+                style = {styles.inputStyleModal}
+                onChangeText={(lastName) => this.setState({last_name: lastName})}
+                value={this.state.last_name}
+                placeholder="Surname"
+                placeholderTextColor={"white"}
+                underlineColorAndroid={'transparent'}
+                />
 
-  Toast.show({
-          text: 'Data successfully added!',
-          position: 'bottom',
-    });
+                <View style={styles.marginTopButton}>
+                <TouchableOpacity
+                  style={styles.modalAddStudent}
+                  onPress={this._saveData}
+                  >
+                      <Text style={styles.addStudentStyleModal}>Add Student</Text>
+                 </TouchableOpacity>
+                 <TouchableOpacity
+                   style={styles.modalAddStudent}
+                   onPress={()=>this._passState()}
+                   >
+                       <Text style={styles.addStudentStyleModal}>Cancel</Text>
+                  </TouchableOpacity>
+                </View>
+               </View>
+             </Modal>
+          </View>
+    );
+  }
+}
+
+class BottomFab extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      active: false,
+      modalView:false,
+    };
+    this._sendData = this._sendData.bind(this);
+    this._resetButtons = this._resetButtons.bind(this);
+    this._handleState = this._handleState.bind(this);
+  }
+  _handleState(childCall){
+    if(childCall == "undefined"){
+      this.setState({
+        modalView: !this.state.modalView
+      });
+    }else{
+      this.setState({
+        modalView: childCall
+      });
+    }
+  }
+
+  _sendData(){
+    console.log("send data");
+    if(student_pal_data.length!=0){
+      for(let i=0 ; i< student_pal_data.length;i++){
+        console.log("student: "+student_pal_data[i].user_id);
+        FB.database().ref("test/"+student_pal_data[i].user_id).set({
+          status: student_pal_data[i]
+        });
+      }
+      Toast.show({
+              text: 'Data successfully added!',
+              position: 'bottom',
+        });
+    }else{
+      Toast.show({
+        text:"can not send empty data!",
+        position: "bottom",
+      });
+    }
+  }
+
+  _resetButtons(){
+    console.log("reset button");
+  }
+
+  render(){
+    return(
+      <View>
+        <StudentModal modalView={this.state.modalView} handleState={this._handleState}/>
+        <Fab
+          active={this.state.active}
+          direction="up"
+          containerStyle={{ }}
+          style={{ backgroundColor: '#5067FF' }}
+          position="bottomRight"
+          onPress={() => this.setState({ active: !this.state.active })}
+          >
+          <Entypo name="plus" color="white" size={22}/>
+          <Button
+            onPress={() => this._handleState()}
+            style={{ backgroundColor: '#5067FF' }}
+            >
+            <Entypo name="add-to-list" color="white" size={22}/>
+          </Button>
+          <Button
+            style={{ backgroundColor: '#5067FF' }}
+            onPress={()=>this._sendData()}
+            >
+            <MaterialIcons name="check" color="white" size={22}/>
+          </Button>
+          <Button
+            style={{ backgroundColor: '#5067FF' }}
+            onPress={()=>this._resetButtons()}
+            >
+            <MaterialIcons name="refresh" color="white" size={22}/>
+          </Button>
+        </Fab>
+      </View>
+    );
+  }
 }
 
 
-_resetButtons(){
-  console.log("reset");
-  this.setState({
-    buttonStyleReset:true,
-    buttonResetActivity:false,
-  });
-  buttonStyle = this.state.buttonStyleReset;
-  buttonState = this.state.buttonResetActivity;
-  //array reset
-  // student_pal_data = [];
-}
 
 
+
+
+
+export default class MainPage extends Component {
+    constructor(props){
+    super(props);
+    //solving timer error
+    console.ignoredYellowBox = [
+    'Setting a timer'
+    ];
+    this.state = {
+
+      students_array: [],
+      active: false,
+      modalView:false,
+    };
+   // this.currentUserUid = FB.auth().currentUser.uid;
+   // this.itemsRef = FB.database().ref('user_classes/'+this.currentUserUid+'/class_list/'+fbDatabaseNodeName+'/studet_list');
+   this.itemsRef = FB.database().ref('user_classes/'+"xuKDcv8itdPnUGhLHjvaWfVEptm2"+'/class_list/'+"First Class"+'/studet_list');
+   this._renderItem = this._renderItem.bind(this);
+   this.listenForItems = this.listenForItems.bind(this);
+
+  }
 //give students reference to the funtion
 componentDidMount() {
   this.listenForItems(this.itemsRef);
@@ -136,8 +262,6 @@ _renderItem({item}){
            userID = {item.id}
            userName = {item.name}
            userSurName = {item.last_name}
-           buttonStyleReset = {this.state.buttonStyleReset}
-           buttonResetActivity = {this.state.buttonResetActivity}
          />
       </Body>
     </ListItem>
@@ -157,13 +281,11 @@ listenForItems(itemsRef) {
     this.setState({students_array: items });
   });
 }
-
   render() {
     return (
       <Container>
         <Content style={{marginBottom:40}}>
-            <List
-              >
+            <List>
               <FlatList
                   style={styles.flatListStyle}
                   data = {this.state.students_array}
@@ -171,78 +293,8 @@ listenForItems(itemsRef) {
                   keyExtractor={item => item.name}
                   />
             </List>
-
-            <View>
-              <Modal
-                animationType="none"
-                transparent={true}
-                visible={this.state.modalView}
-                onRequestClose={()=>this.setState({modalVisible: false})}
-              >
-              <View style={styles.inputcontainerModal}>
-                <TextInput
-                  style = {styles.inputStyleModal}
-                  onChangeText={(Name) => this.setState({name: Name})}
-                  value={this.state.name}
-                  placeholder="Name"
-                  placeholderTextColor={"white"}
-                  underlineColorAndroid={'transparent'}
-                  />
-                <TextInput
-                  style = {styles.inputStyleModal}
-                  onChangeText={(lastName) => this.setState({last_name: lastName})}
-                  value={this.state.last_name}
-                  placeholder="Surname"
-                  placeholderTextColor={"white"}
-                  underlineColorAndroid={'transparent'}
-                  />
-
-                  <View style={styles.marginTopButton}>
-                  <TouchableOpacity
-                    style={styles.modalAddStudent}
-                    onPress={this._saveData}
-                    >
-                        <Text style={styles.addStudentStyleModal}>Add Student</Text>
-                   </TouchableOpacity>
-                   <TouchableOpacity
-                     style={styles.modalAddStudent}
-                     onPress={()=>{this.setState({modalView: false})}}
-                     >
-                         <Text style={styles.addStudentStyleModal}>Cancel</Text>
-                    </TouchableOpacity>
-                  </View>
-                 </View>
-               </Modal>
-            </View>
       </Content>
-        <Fab
-          active={this.state.active}
-          direction="up"
-          containerStyle={{ }}
-          style={{ backgroundColor: '#5067FF' }}
-          position="bottomRight"
-          onPress={() => this.setState({ active: !this.state.active })}
-          >
-          <Entypo name="plus" color="white" size={22}/>
-          <Button
-            onPress={() => this.setState({ modalView: !this.state.modalView})}
-            style={{ backgroundColor: '#5067FF' }}
-            >
-            <Entypo name="add-to-list" color="white" size={22}/>
-          </Button>
-          <Button
-            style={{ backgroundColor: '#5067FF' }}
-            onPress={()=>this._sendData()}
-            >
-            <MaterialIcons name="check" color="white" size={22}/>
-          </Button>
-          <Button
-            style={{ backgroundColor: '#5067FF' }}
-            onPress={()=>this._resetButtons()}
-            >
-            <MaterialIcons name="refresh" color="white" size={22}/>
-          </Button>
-        </Fab>
+      <BottomFab />
     </Container>
     );
   }
